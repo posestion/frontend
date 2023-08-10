e00a8e2dc9789a43939331c5cb30ad1b56c42c13
DESKTOP-FSRVJ63\gram <20kimkayeon@gmail.com>
2023-08-10 15:06:59 +0900

    레이아웃이랑 일부 메뉴 수정

--- app/src/main/java/com/example/posestion/MyFragment3.kt
+++ app/src/main/java/com/example/posestion/MyFragment3.kt
@@ -2,6 +2,7 @@ package com.example.posestion
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,18 +10,30 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 
 class MyFragment3 : Fragment() {
+    private lateinit var sharedViewModel: SharedViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+    }
+
+    private fun onHeartButtonClick(imageId: Int) {
+        sharedViewModel.addNewImage(imageId)
+        Log.d(imageId.toString(),"bbb")
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_layout_3, container, false)
 
         // 새로운 이미지뷰를 찾습니다. (XML에서 적절한 ID를 지정해야 합니다.)
         val newImageView = rootView.findViewById<View>(R.id.imageView)
+        val newImageViewId = R.drawable.rectangle_67
         // 새로운 이미지뷰에 클릭 리스너를 등록합니다.
         newImageView.setOnClickListener {
             // 이미지를 크게 보여주는 AlertDialog를 표시합니다.
-            showLargeImageDialog()
+            showLargeImageDialog(newImageViewId)
         }
         val buttonEvent = rootView.findViewById<View>(com.example.posestion.R.id.heartButton1)
         var isButtonFilled = false
@@ -29,17 +42,28 @@ class MyFragment3 : Fragment() {
             isButtonFilled = !isButtonFilled
             if (isButtonFilled) {
                 buttonEvent.setBackgroundResource(com.example.posestion.R.drawable.fillheart)
+                onHeartButtonClick(R.drawable.rectangle_67)
             } else {
                 buttonEvent.setBackgroundResource(com.example.posestion.R.drawable._icon__heart_)
+                sharedViewModel.removeImage(R.drawable.rectangle_67)
             }
         }
 
+        sharedViewModel.removedImage.observe(viewLifecycleOwner) { removedImageId ->
+            Log.d("removedImage ID2", removedImageId.toString())
+            Log.d("removedImage ID22", newImageViewId.toString())
+            if (removedImageId == newImageViewId) { // 이미지뷰의 ID를 직접 비교합니다.
+                buttonEvent.setBackgroundResource(com.example.posestion.R.drawable._icon__heart_)
+                isButtonFilled = false
+            }
+        }
 
         val newImageView2 = rootView.findViewById<View>(com.example.posestion.R.id.imageView2)
+        val newImageViewId2 = R.drawable.rectangle_67
         // 새로운 이미지뷰에 클릭 리스너를 등록합니다.
         newImageView2.setOnClickListener {
             // 이미지를 크게 보여주는 AlertDialog를 표시합니다.
-            showLargeImageDialog()
+            showLargeImageDialog(newImageViewId2)
         }
 
         val buttonEvent2 = rootView.findViewById<View>(com.example.posestion.R.id.heartButton2)
@@ -49,16 +73,19 @@ class MyFragment3 : Fragment() {
             isButtonFilled2 = !isButtonFilled2
             if (isButtonFilled2) {
                 buttonEvent2.setBackgroundResource(com.example.posestion.R.drawable.fillheart)
+                onHeartButtonClick(R.drawable.rectangle_67)
             } else {
                 buttonEvent2.setBackgroundResource(com.example.posestion.R.drawable._icon__heart_)
+                sharedViewModel.removeImage(R.drawable.rectangle_67)
             }
         }
 
         val newImageView3 = rootView.findViewById<View>(com.example.posestion.R.id.imageView3)
+        val newImageViewId3 = R.drawable.rectangle_67
         // 새로운 이미지뷰에 클릭 리스너를 등록합니다.
         newImageView3.setOnClickListener {
             // 이미지를 크게 보여주는 AlertDialog를 표시합니다.
-            showLargeImageDialog()
+            showLargeImageDialog(newImageViewId3)
         }
         val buttonEvent3 = rootView.findViewById<View>(com.example.posestion.R.id.heartButton3)
         var isButtonFilled3 = false
@@ -67,16 +94,19 @@ class MyFragment3 : Fragment() {
             isButtonFilled3 = !isButtonFilled3
             if (isButtonFilled3) {
                 buttonEvent3.setBackgroundResource(com.example.posestion.R.drawable.fillheart)
+                onHeartButtonClick(R.drawable.rectangle_67)
             } else {
                 buttonEvent3.setBackgroundResource(com.example.posestion.R.drawable._icon__heart_)
+                sharedViewModel.removeImage(R.drawable.rectangle_67)
             }
         }
 
         val newImageView4 = rootView.findViewById<View>(com.example.posestion.R.id.imageView4)
+        val newImageViewId4 = R.drawable.rectangle_67
         // 새로운 이미지뷰에 클릭 리스너를 등록합니다.
         newImageView4.setOnClickListener {
             // 이미지를 크게 보여주는 AlertDialog를 표시합니다.
-            showLargeImageDialog()
+            showLargeImageDialog(newImageViewId4)
         }
         val buttonEvent4 = rootView.findViewById<View>(com.example.posestion.R.id.heartButton4)
         var isButtonFilled4 = false
@@ -85,16 +115,19 @@ class MyFragment3 : Fragment() {
             isButtonFilled4 = !isButtonFilled4
             if (isButtonFilled4) {
                 buttonEvent4.setBackgroundResource(com.example.posestion.R.drawable.fillheart)
+                onHeartButtonClick(R.drawable.rectangle_67)
             } else {
                 buttonEvent4.setBackgroundResource(com.example.posestion.R.drawable._icon__heart_)
+                sharedViewModel.removeImage(R.drawable.rectangle_67)
             }
         }
 
         val newImageView5 = rootView.findViewById<View>(com.example.posestion.R.id.imageView5)
+        val newImageViewId5 = R.drawable.rectangle_67
         // 새로운 이미지뷰에 클릭 리스너를 등록합니다.
         newImageView5.setOnClickListener {
             // 이미지를 크게 보여주는 AlertDialog를 표시합니다.
-            showLargeImageDialog()
+            showLargeImageDialog(newImageViewId5)
         }
         val buttonEvent5 = rootView.findViewById<View>(com.example.posestion.R.id.heartButton5)
         var isButtonFilled5 = false
@@ -103,8 +136,10 @@ class MyFragment3 : Fragment() {
             isButtonFilled5 = !isButtonFilled5
             if (isButtonFilled5) {
                 buttonEvent5.setBackgroundResource(com.example.posestion.R.drawable.fillheart)
+                onHeartButtonClick(R.drawable.rectangle_67)
             } else {
                 buttonEvent5.setBackgroundResource(com.example.posestion.R.drawable._icon__heart_)
+                sharedViewModel.removeImage(R.drawable.rectangle_67)
             }
         }
 
@@ -113,13 +148,14 @@ class MyFragment3 : Fragment() {
     }
 
 
-    private fun showLargeImageDialog() {
-        // 커스텀 레이아웃을 인플레이트합니다.
+    private val showLargeImageDialog: (Int) -> Unit = { imageId ->
+        // 이미지를 크게 보여주는 AlertDialog를 표시하는 로직을 작성
         val inflater = LayoutInflater.from(requireContext())
         val dialogView = inflater.inflate(R.layout.dialog_large_image, null)
 
         // 커스텀 레이아웃의 이미지뷰를 찾습니다.
         val largeImageView = dialogView.findViewById<ImageView>(R.id.largeImageView)
+        largeImageView.setImageResource(imageId) // 이미지 설정
 
         // AlertDialog를 생성합니다.
         val builder = AlertDialog.Builder(requireContext())
--- app/src/main/java/com/example/posestion/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/posestion/MyRecyclerViewAdapter.kt
@@ -5,20 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.posestion.databinding.ItemRecyclerviewBinding
 
-class MyRecyclerViewAdapter:  RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder>(){
-    private val dataSet: ArrayList<List<String>> = arrayListOf<List<String>>().apply {
-        for (i in 1..5) {
-            add(listOf("$i", "image_${i % 8}")) // 이미지 리소스 이름 추가
-        }
-    }
+class MyRecyclerViewAdapter():  RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder>(){
+    private val dataSet: ArrayList<List<String>> = arrayListOf()
+    private val addedImageIds: MutableSet<Int> = mutableSetOf()
 
     fun removeData(position: Int) {
-        dataSet.removeAt(position)
-        notifyItemRemoved(position)
+        if (position in 0 until dataSet.size) {
+            dataSet.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, dataSet.size) // 변경된 위치 이후의 아이템들을 다시 그리도록 알려줌
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,7 +28,12 @@ class MyRecyclerViewAdapter:  RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHol
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(dataSet[position], position)
+        Log.d("MyRecyclerViewAdapter3", addedImageIds.toString())
+        val imageId: Int? = addedImageIds.elementAtOrNull(position)
+
+        if (imageId != null) {
+            holder.bind(imageId, position)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -42,21 +48,37 @@ class MyRecyclerViewAdapter:  RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHol
 
         init {
             // 이미지 리소스 설정 (이미지 설정은 ViewHolder가 생성될 때 한 번만 이루어집니다.)
-            binding.imageView.setImageResource(R.drawable.baseline_square_24)
+            binding.imageView.setImageResource(R.drawable.rectangle_67)
         }
 
-        fun bind(data: List<String>, position: Int) {
-            binding.tvMain.text = data[0]
+        fun bind(imageId: Int, position: Int) {
+            binding.tvMain.text = "${position + 1}"
 
             binding.imageButton1.setOnClickListener {
-                // PopupMenu를 생성하고 메뉴를 표시합니다.
-                // 이제 인터페이스를 통해 showPopupMenu 함수를 호출합니다.
                 popupMenuClickListener?.onPopupMenuClick(it, position)
             }
-
-            // 이미지 리소스 설정
-            gridAdapter.setImage(R.drawable.baseline_square_24)
+            binding.imageView.setImageResource(imageId)
         }
+    }
+    fun addPose(poseImageResource: Int) {
+        if (addedImageIds.contains(poseImageResource)) {
+            return // 이미 추가된 이미지 ID일 경우 중복 추가 방지
+        }
+
+        // 이미지 아이디를 추가하고 중복 확인
+        addedImageIds.add(poseImageResource)
+        Log.d("MyRecyclerViewAdapter2",  addedImageIds.toString())
+        // 데이터 중복 확인 후 추가
+        val newData = listOf("${dataSet.size + 1}", "image_${poseImageResource % 8}")
+        if (!dataSet.contains(newData)) {
+            dataSet.add(newData)
+            Log.d("MyRecyclerViewAdapter", dataSet.toString())
+            notifyItemInserted(dataSet.size - 1)
+        }
+    }
+
+    fun getItemData(position: Int): List<String> {
+        return dataSet[position]
     }
 
     // 인터페이스를 정의하여 클릭 이벤트를 처리할 수 있도록 합니다.
