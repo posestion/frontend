218e743960f8a55a8651e952b583bb25ee70ef27
newoldname <thjbjs@outlook.com>
2023-08-25 17:00:41 +0900

    [feat] 카메라 기본 기능 완성

--- app/src/main/java/com/example/posestion/camera/RatioType.kt
+++ app/src/main/java/com/example/posestion/camera/RatioType.kt
@@ -8,18 +8,18 @@ enum class RatioType{
     },
     Ratio4_3 {
         override fun next(): RatioType {
-            return Ratio1_1
-        }
-    },
-    Ratio1_1 {
-        override fun next(): RatioType {
-            return Ratio_full
-        }
-    },
-    Ratio_full {
-        override fun next(): RatioType {
             return Ratio9_16
         }
     };
+//    Ratio1_1 {
+//        override fun next(): RatioType {
+//            return Ratio_full
+//        }
+//    },
+//    Ratio_full {
+//        override fun next(): RatioType {
+//            return Ratio9_16
+//        }
+//    };
     abstract fun next(): RatioType
 }
