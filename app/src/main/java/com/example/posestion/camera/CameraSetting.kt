5c9cad2251a9da9af86617453aa31efa73417834
newoldname <thjbjs@outlook.com>
2023-08-30 14:56:46 +0900

    [feat] 카메라 설정 함수 완성

--- app/src/main/java/com/example/posestion/camera/CameraSetting.kt
+++ app/src/main/java/com/example/posestion/camera/CameraSetting.kt
@@ -5,15 +5,45 @@ import android.content.SharedPreferences
 
 class CameraSetting(context: Context) {
     private val prefs: SharedPreferences =
-        context.getSharedPreferences("prefs_name", Context.MODE_PRIVATE)
+        context.getSharedPreferences("camera_setting", Context.MODE_PRIVATE)
 
-//    fun getMarkSwitchStatus(): String {
-//        return prefs.getString(key, defValue).toString()
-//    }
-//
-//
-//
-//    fun setString(key: String, str: String) {
-//        prefs.edit().putString(key, str).apply()
-//    }
+    fun getMarkSwitchStatus(): Boolean {
+        return prefs.getBoolean("mark_switch", false)
+    }
+
+    fun setMarkSwitchStatus(stat: Boolean) {
+        prefs.edit().putBoolean("mark_switch", stat).apply()
+    }
+
+    fun getMarkDateStatus(): Boolean {
+        return prefs.getBoolean("mark_date_switch", false)
+    }
+
+    fun setMarkDateStatus(stat: Boolean) {
+        prefs.edit().putBoolean("mark_date_switch", stat).apply()
+    }
+
+    fun getMarkNicknameStatus(): Boolean {
+        return prefs.getBoolean("mark_nickname_switch", false)
+    }
+
+    fun setMarkNicknameStatus(stat: Boolean) {
+        prefs.edit().putBoolean("mark_nickname_switch", stat).apply()
+    }
+
+    fun getMarkNicknameString(): String {
+        return prefs.getString("mark_nickname_string", "").toString()
+    }
+
+    fun setMarkNicknameString(stat: String) {
+        prefs.edit().putString("mark_nickname_string", stat).apply()
+    }
+
+    fun getLineSwitchStatus(): Boolean {
+        return prefs.getBoolean("line_switch", false)
+    }
+
+    fun setLineSwitchStatus(stat: Boolean) {
+        prefs.edit().putBoolean("line_switch", stat).apply()
+    }
 }
